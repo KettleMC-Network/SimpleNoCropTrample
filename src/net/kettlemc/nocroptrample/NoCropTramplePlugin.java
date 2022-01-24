376739f62d0fe3ebf8e7f210cc166a75127886da
UeberallGebannt <paulmertens63@gmail.com>
2022-01-24 20:12:10 +0100

    Add bstats

--- src/net/kettlemc/nocroptrample/NoCropTramplePlugin.java
+++ src/net/kettlemc/nocroptrample/NoCropTramplePlugin.java
@@ -2,16 +2,24 @@ package net.kettlemc.nocroptrample;
 
 import net.kettlemc.nocroptrample.configuration.Configuration;
 import net.kettlemc.nocroptrample.listener.TrampleListener;
+import net.kettlemc.nocroptrample.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoCropTramplePlugin extends JavaPlugin {
-	
+
+	private static final int PLUGIN_METRICS_ID = 14051;
+
 	private Configuration configuration;
 	
 	public void onEnable() {
 		this.configuration = new Configuration();
 		this.registerBukkit();
+		this.startMetrics();
+	}
+
+	private void startMetrics() {
+		new Metrics(this, PLUGIN_METRICS_ID);
 	}
 
 	public Configuration getConfiguration() {
